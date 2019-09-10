44b4d48a56fe269767f42a158989969f533757b2
Venkat Yarlagadda <venkat.yarl@gmail.com>
2019-09-09 22:56:18 -0500

    Initial Commit after creating the Spring Start project

---
+++ src/main/java/com/venkatyarlagadda/SpringAsynMethod/SpringAsynMethodApplication.java
@@ -0,0 +1,13 @@
+package com.venkatyarlagadda.SpringAsynMethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsynMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsynMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venkatyarlagadda/SpringAsynMethod/SpringAsynMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatyarlagadda.SpringAsynMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsynMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
